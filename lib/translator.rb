b74f5d5ecb207ced4b7c6684aa88566f3800b0c0
Brian Kang <brianhkang1@gmail.com>
2018-08-20 20:11:24 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,13 @@
 require "yaml"
 
 def load_library(file_path)
-  YAML.load_file(file_path)
+  new_hash = {}
+  yaml_hash = YAML.load_file(file_path)
+  
+  new_hash["get meaning"] = {}
+  new_hash["get_emoticon"] = {}
+  
+  yaml_hash.each do |
 end
 
 def get_japanese_emoticon
