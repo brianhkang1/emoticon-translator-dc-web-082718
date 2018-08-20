27441f1f5e3bd1abced6369c0817ca266d31647f
Brian Kang <brianhkang1@gmail.com>
2018-08-20 20:37:34 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,4 +20,11 @@ def load_library(file_path)
   new_hash
 end
 
-puts load_library("./lib/emoticons.yml")
+
+def get_japanese_emoticon
+  # code goes here
+end
+
+def get_english_meaning
+  # code goes here
+end
