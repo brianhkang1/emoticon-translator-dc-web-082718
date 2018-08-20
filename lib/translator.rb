807d94d4cccffd4f040f101b19cc620904cd48e5
Brian Kang <brianhkang1@gmail.com>
2018-08-20 20:30:28 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ def load_library(file_path)
     end
     
     if !new_hash["get_emoticon"].has_key?(value[0])
-      new_hash["get_emoticon"][value[0]] = key
+      new_hash["get_emoticon"][value[0]] = yaml_hash
     end 
   end 
   
